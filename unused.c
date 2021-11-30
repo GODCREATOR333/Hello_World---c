4f5b17d657dea1e5fe9e67fc8181eebb77a8492e
GODCREATOR333 <preetham.chitti.9@gmail.com>
2021-11-30 06:45:29 +0530

    replaced old files

---
+++ unused.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  
+  int num1 ;
+  int numb2 ;
+  printf("enter number1 : \n");
+  scanf("lf",num1)
+  printf("enter number2 : \n");
+  scanf("lf",num2);
+  printf("answer: %f",num1+num2 );
+
+
+
+
+  return 0; 
+  
+  
+}
