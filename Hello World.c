eaacac41fba63ba6c6ce3b191d4ebb67e02b50f5
GODCREATOR333 <preetham.chitti.9@gmail.com>
2021-11-30 06:31:18 +0530

    replaced file learned used input method

---
+++ Hello World.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int age; 
+  printf("tell me your age: \n");
+  scanf("%d",age);
+  printf("so you are %d years old\n",age);
+  return 0; 
+  
+  
+}
